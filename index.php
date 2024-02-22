407887ab127dd918c4b4f84e3d056223ebe1b05c
Rajan Sharma <82056580+Immortal-codes@users.noreply.github.com>
2024-02-22 10:32:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include('includes/header.php') ?>
+
+
+<h1>hello</h1>
+
+
+<button class="btn btn-primary">Testing</button>
+
+
+<?php include('includes/footer.php') ?>
---
+++ register.php
@@ -0,0 +1,7 @@
+<?php include('includes/header.php') ?>
+
+
+<h4>Registraion Form</h4>
+
+
+<?php include('includes/footer.php') ?>
